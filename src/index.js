7bd0e21362e8d2334da74aa3adc850e6fbac0213
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-30 15:37:43 -0400

    Done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
+      <h1>Home</h1>
     </div>
   );
 };
 
 ReactDOM.render(
-  <App />, 
+  <Router>
+    <Route path='/' component={App} />
+  </Router>,
   document.getElementById('root')
 );
